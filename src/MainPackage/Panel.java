75da74868f35d13ddbe6511a62841ae8860509f8
Auqherus <patrykromanowski@gmail.com>
2022-06-23 11:59:23 +0200

    First version of project is finished, need to add some stuff like START GAME button etc.
    + added Start Game 23.06.2022

--- src/MainPackage/Panel.java
+++ src/MainPackage/Panel.java
@@ -15,12 +15,13 @@ public class Panel extends JPanel implements ActionListener {
     // ustawienia planszy
     final int tabX[] = new int[GAME_UNITS]; // dla węża(głowa, body etc)
     final int tabY[] = new int[GAME_UNITS];
-    int bodyParts = 6;
+    int bodyParts = 3;
     int eatenApples;
     int appleX;
     int appleY;
     char direction = 'R';
     boolean running = false;
+    boolean isGameMenu = true;
     Timer timer;
     Random random;
 
@@ -36,6 +37,7 @@ public class Panel extends JPanel implements ActionListener {
     }
     public void startGame(){
         newApple();
+        newSnakePosition();
         running = true;
         timer = new Timer(DELAY,this);
         timer.start();
@@ -46,13 +48,26 @@ public class Panel extends JPanel implements ActionListener {
     }
 
     public void draw(Graphics g) {
-        if (running) {
+        if(isGameMenu){
+
+            g.setColor(Color.GREEN); // metoda dla koniec gry
+            g.setFont(new Font("Comic Sans MS",Font.BOLD,36));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Press Enter to start Game!",(SCREEN_WIDTH)/UNIT_SIZE+25,SCREEN_HEIGHT-450);
+
+            g.setColor(new Color(48, 217, 36, 255)); //podpis autora
+            g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
+            FontMetrics metrics2 = getFontMetrics(g.getFont());
+            g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+UNIT_SIZE*9-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10); // for author
+            /*g.drawString("Game made by Auqherus",SCREEN_WIDTH-120,SCREEN_HEIGHT-10);*/
+        }
+       else if (running && !isGameMenu) {
             /*for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT); //ustawienie siatki na X
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE); //ustawienie siatki na Y
             }*/
                 g.setColor(Color.RED);
-                g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE); // ustawienie jabka
+                g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE); // wygląd jabłka
                 for (int i = 0; i < bodyParts; i++) { // cialo weza
                 if (i == 0) {
                     g.setColor(Color.GREEN);
@@ -66,28 +81,35 @@ public class Panel extends JPanel implements ActionListener {
             g.setColor(new Color(182, 36, 217, 255));
             g.setFont(new Font("Comic Sans MS",Font.BOLD,20));
             FontMetrics metrics = getFontMetrics(g.getFont());
-            g.drawString("Score : "+eatenApples,(SCREEN_WIDTH - metrics.stringWidth("Score : "+eatenApples))/2,g.getFont().getSize()); // for score
+            g.drawString("Score : "+eatenApples,(SCREEN_WIDTH - metrics.stringWidth("Score : "+eatenApples))/2,g.getFont().getSize()); // wynik
 
             g.setColor(new Color(48, 217, 36, 255)); //podpis autora
             g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
             FontMetrics metrics2 = getFontMetrics(g.getFont());
-            g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+125-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10); // for author
+            g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+UNIT_SIZE*9-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10); // for author
             /*g.drawString("Game made by Auqherus",SCREEN_WIDTH-120,SCREEN_HEIGHT-10);*/
         }
         else{
             gameOver(g);
+
+
         }
     }
     public void newApple(){
-        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;   // gdzie ma się pojawić jabłko
         appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
     }
+
+    public void newSnakePosition(){
+        tabX[0] = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        tabY[0] = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
     public void moves(){
-        for (int i = bodyParts; i>0; i--) {
+        for (int i = bodyParts; i>0; i--) {  // animowanie ciała węża - podążaj za ciałem
             tabX[i] =tabX[i-1];
             tabY[i] =tabY[i-1];
         }
-        switch(direction){
+        switch(direction){ // animowanie głowy węża
             case 'U':
                 tabY[0] = tabY[0] - UNIT_SIZE;
                 break;
@@ -103,10 +125,10 @@ public class Panel extends JPanel implements ActionListener {
         }
     }
     public void lookForApple(){
-        if(tabX[0] == appleX && tabY[0] == appleY){
-            bodyParts++;
-            eatenApples++;
-            newApple();
+        if(tabX[0] == appleX && tabY[0] == appleY){   // jeżeli głowa węża będzie na pozycji jabłka :
+            bodyParts++;                     // zwiększ dlugość węża +1
+            eatenApples++;                  // nalicz wynik +1
+            newApple();                     // generuj nowe jabłko
         }
     }
     public void checkCollisions(){
@@ -145,10 +167,19 @@ public class Panel extends JPanel implements ActionListener {
         g.setColor(new Color(48, 217, 36, 255)); //podpis autora
         g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
         FontMetrics metrics2 = getFontMetrics(g.getFont());
-        g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+125-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10); // for author
+        g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+UNIT_SIZE*9-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10); // for author
+    }
+
+    public void startNewGame(Graphics g){
+
+        g.setColor(new Color(48, 217, 36, 255)); //podpis autora
+        g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+UNIT_SIZE*9-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10);
+
     }
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) {  // główne metody metody kolizji, jabłka i ruchu
 
         if(running){
             moves();
@@ -160,7 +191,7 @@ public class Panel extends JPanel implements ActionListener {
     }
     public class MyKeyAdapter extends KeyAdapter{
         @Override
-        public void keyPressed(KeyEvent e){
+        public void keyPressed(KeyEvent e){     // przypisanie ruchów węża do klawiatury
             switch (e.getKeyCode()){
                 case KeyEvent.VK_LEFT:
                     if(direction != 'R'){
@@ -182,7 +213,19 @@ public class Panel extends JPanel implements ActionListener {
                         direction = 'D';
                     }
                     break;
+                case KeyEvent.VK_ENTER:
+                    if(isGameMenu && !running){
+                        isGameMenu = false;
+                        startGame();
+                    }
+                    else if(running){
+                        break;
+                    }
+
+
+
             }
+
         }
     }
 }
