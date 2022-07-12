31efa599edc0215b4cf7fc6856eb3dbe5f3fcd79
Auqherus <patrykromanowski@gmail.com>
2022-07-12 23:18:12 +0200

    First version of project is finished, need to add some stuff like START GAME button etc.
    + added Start Game 23.06.2022
    + added Try again! at Game Over screen 23.06.2022
    + added new apple and snake viev 23.06.2022
    + changed the size of window 07.07.2022
    + snake now always Start from the middle 07.07.2022
    + added Upqraded Menu
    + added PAUSE

--- src/MainPackage/Panel.java
+++ src/MainPackage/Panel.java
@@ -1,19 +1,18 @@
 package MainPackage;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
+import java.io.File;
 import java.util.Random;
-
 
 public class Panel extends JPanel implements ActionListener {
     // Ustawienia panelu dla gry
-    static final int SCREEN_WIDTH = 750;
-    static final int SCREEN_HEIGHT = 750;
-    static final int UNIT_SIZE = 25;
-    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-    static int DELAY = 72;
+    static final int SCREEN_WIDTH = 720;
+    static final int SCREEN_HEIGHT = 720;
+    static final int UNIT_SIZE = 20;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
+    static int DELAY = 65;
     // ustawienia planszy
     final int tabX[] = new int[GAME_UNITS]; // dla węża(głowa, body etc)
     final int tabY[] = new int[GAME_UNITS];
@@ -25,113 +24,176 @@ public class Panel extends JPanel implements ActionListener {
     boolean running = false;
     boolean isGameMenu = true;
     boolean isGameOver = false;
-    boolean isBoostMode = false;
-    Image appleFood = new Image() {
-        @Override
-        public int getWidth(ImageObserver observer) {
-            return UNIT_SIZE;
-        }
+    File appleFile = new File("apple.png");
+    /*File bodyFile = new File("body2.png");*/
+    boolean pause = false;
 
-        @Override
-        public int getHeight(ImageObserver observer) {
-            return UNIT_SIZE;
-        }
-
-        @Override
-        public ImageProducer getSource() {
-            return null;
-        }
-
-        @Override
-        public Graphics getGraphics() {
-            return null;
-        }
-
-        @Override
-        public Object getProperty(String name, ImageObserver observer) {
-            return null;
-        }
-    };
     Timer timer;
     Random random;
 
-
-    Panel(){
+    Panel() {
         random = new Random();
-        this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
         startGame();
 
     }
-    public void startGame(){
+
+    public void startGame() {
         newApple();
         newSnakePosition();
         running = true;
-        timer = new Timer(DELAY,this);
+        timer = new Timer(DELAY, this);
         timer.start();
     }
 
-    public void paintComponent(Graphics g){
-     super.paintComponent(g);
-     draw(g);
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
     }
 
-    public void draw(Graphics g) {
-        if(isGameMenu){
+    public void gamePause(Graphics g) {
+        timer.stop();
+        g.setColor(Color.GREEN);
+        g.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("PAUSE!", (SCREEN_WIDTH - metrics2.stringWidth("PAUSE!")) / 2, SCREEN_HEIGHT - 450);
 
-            g.setColor(Color.GREEN); // metoda dla koniec gry
-            g.setFont(new Font("Comic Sans MS",Font.BOLD,36));
-            /*FontMetrics metrics = getFontMetrics(g.getFont());*/
-            g.drawString("Press Enter to start Game!",(SCREEN_WIDTH)/UNIT_SIZE+125,SCREEN_HEIGHT-550);
+        g.setColor(new Color(182, 36, 217, 255));
+        g.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Score : " + eatenApples, (SCREEN_WIDTH - metrics.stringWidth("Score : " + eatenApples)) / 2, g.getFont().getSize()); // wynik
 
-            g.setColor(new Color(48, 217, 36, 255)); //podpis autora
-            g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
-            /*FontMetrics metrics2 = getFontMetrics(g.getFont());*/
-            g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH-305),SCREEN_HEIGHT-10); // for author
-            /*g.drawString("Game made by Auqherus",SCREEN_WIDTH-120,SCREEN_HEIGHT-10);*/
+        g.setColor(new Color(48, 217, 36, 255)); //podpis autora
+        g.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+        g.drawString("Game made by Auqherus na zaliczenie projektu", (SCREEN_WIDTH - 305), SCREEN_HEIGHT - 10); // for author
+
+
+        try {
+            Image appleFoodImg = ImageIO.read(appleFile);
+            g.drawImage(appleFoodImg, appleX, appleY, UNIT_SIZE, UNIT_SIZE, this); // JABLKO JAKO OBRAZEK!
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-       else if (running) {
+
+        for (int i = 0; i < bodyParts; i++) { // cialo weza
+            if (i == 0) {
+                g.setColor(Color.GREEN);
+                g.fillRect(tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE); // glowa wyglad
+
+            } else {
+                g.setColor(new Color(36, 138, 26));
+                g.fillRect(tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE); // cialo wyglad
+
+            }
+        }
+    }
+
+
+
+    public void draw(Graphics g) {
+
+        if (pause && !isGameMenu) {
+            gamePause(g);
+
+        } else {
+            if (isGameMenu) {
+
+                g.setColor(Color.GREEN); // metoda dla START GAME
+                g.setFont(new Font("Comic Sans MS", Font.BOLD, 36));
+                FontMetrics metrics = getFontMetrics(g.getFont());
+                g.drawString("Press Enter to start Game!", (SCREEN_WIDTH - metrics.stringWidth("Press Enter to start Game!")) / 2, SCREEN_HEIGHT - 550);
+
+                g.setColor(Color.GREEN); // Pause
+                g.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+                FontMetrics metrics2 = getFontMetrics(g.getFont());
+                g.drawString("Press [P] to PAUSE!", (SCREEN_WIDTH - metrics2.stringWidth("Press [P] to PAUSE!")) / 2, SCREEN_HEIGHT - 450);
+
+                g.setColor(Color.GREEN); // arrows
+                g.setFont(new Font("Comic Sans MS", Font.BOLD, 26));
+                FontMetrics metrics3 = getFontMetrics(g.getFont());
+                g.drawString("Use [<] [^] [>] to MOVE", (SCREEN_WIDTH - metrics3.stringWidth("Use [<] [^] [>] to MOVE")) / 2, SCREEN_HEIGHT - 350);
+                /*FontMetrics metrics = getFontMetrics(g.getFont());*/
+
+                g.setColor(new Color(48, 217, 36, 255)); //podpis autora
+                g.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+                g.drawString("Game made by Auqherus na zaliczenie projektu", (SCREEN_WIDTH - 305), SCREEN_HEIGHT - 10); // for author
+                /*g.drawString("Game made by Auqherus",SCREEN_WIDTH-120,SCREEN_HEIGHT-10);*/
+            } else if (running) {
             /*for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
                 g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT); //ustawienie siatki na X
                 g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE); //ustawienie siatki na Y
             }*/
-                g.setColor(Color.ORANGE);
+                isGameMenu = false;
+                /* g.setColor(Color.ORANGE);*/
                 /*g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE); // wygląd jabłka*/
-                g.fillArc(appleX,appleY,UNIT_SIZE,UNIT_SIZE,15,225); // rogalik zamiast jabłka ^^
+                /* g.fillArc(appleX,appleY,UNIT_SIZE,UNIT_SIZE,15,225);*/ // rogalik zamiast jabłka ^^
+
+                try {
+                    Image appleFoodImg = ImageIO.read(appleFile);
+                    g.drawImage(appleFoodImg, appleX, appleY, UNIT_SIZE, UNIT_SIZE, this); // JABLKO JAKO OBRAZEK!
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
 
                 for (int i = 0; i < bodyParts; i++) { // cialo weza
-                if (i == 0) {
-                    g.setColor(Color.GREEN);
-                    g.fillOval(tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE);
-                } else {
-                    g.setColor(new Color(36, 138, 26));
-                   /* g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255),random.nextInt(255)));*/
-                    g.fillOval(tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE);
+                    if (i == 0) {
+                        g.setColor(Color.GREEN);
+                        g.fillRect(tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE); // glowa wyglad
+                    /*try {
+
+                        Image bodyImage = ImageIO.read(bodyFile);
+
+                        g.drawImage(bodyImage, tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE, this);
+                         // Kolorowe cialo!
+                        }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                    }*/
+                    } else {
+                        g.setColor(new Color(36, 138, 26));
+                        /* g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255),random.nextInt(255)));*/
+                        g.fillRect(tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE); // cialo wyglad
+                    /*try {
+
+                        Image bodyImage = ImageIO.read(bodyFile);
+
+                        g.drawImage(bodyImage, tabX[i], tabY[i], UNIT_SIZE, UNIT_SIZE, this);
+                        // Kolorowe cialo!
+                        }
+                    catch (Exception e) {
+                        e.printStackTrace();
+                        }*/
+                    }
                 }
+                g.setColor(new Color(182, 36, 217, 255));
+                g.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+                FontMetrics metrics = getFontMetrics(g.getFont());
+                g.drawString("Score : " + eatenApples, (SCREEN_WIDTH - metrics.stringWidth("Score : " + eatenApples)) / 2, g.getFont().getSize()); // wynik
+
+                g.setColor(new Color(48, 217, 36, 255)); //podpis autora
+                g.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+                /*FontMetrics metrics2 = getFontMetrics(g.getFont());*/
+                g.drawString("Game made by Auqherus na zaliczenie projektu", (SCREEN_WIDTH - 305), SCREEN_HEIGHT - 10); // for author
+                /*g.drawString("Game made by Auqherus",SCREEN_WIDTH-120,SCREEN_HEIGHT-10);*/
+
+            } else {
+                gameOver(g);
+                isGameOver = true;
             }
-            g.setColor(new Color(182, 36, 217, 255));
-            g.setFont(new Font("Comic Sans MS",Font.BOLD,20));
-            FontMetrics metrics = getFontMetrics(g.getFont());
-            g.drawString("Score : "+eatenApples,(SCREEN_WIDTH - metrics.stringWidth("Score : "+eatenApples))/2,g.getFont().getSize()); // wynik
-
-            g.setColor(new Color(48, 217, 36, 255)); //podpis autora
-            g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
-            /*FontMetrics metrics2 = getFontMetrics(g.getFont());*/
-            g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH-305),SCREEN_HEIGHT-10); // for author
-            /*g.drawString("Game made by Auqherus",SCREEN_WIDTH-120,SCREEN_HEIGHT-10);*/
-        }
-        else{
-            gameOver(g);
-            isGameOver = true;
-
         }
     }
-    public void newApple(){
-        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;   // gdzie ma się pojawić jabłko
-        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+
+
+
+    public void newApple() {
+
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;   // gdzie ma się pojawić jabłko
+        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
+
 
     public void newSnakePosition(){
         tabX[0] = UNIT_SIZE*15;
@@ -142,19 +204,11 @@ public class Panel extends JPanel implements ActionListener {
             tabX[i] =tabX[i-1];
             tabY[i] =tabY[i-1];
         }
-        switch(direction){ // animowanie głowy węża
-            case 'U':
-                tabY[0] = tabY[0] - UNIT_SIZE;
-                break;
-            case 'D':
-                tabY[0] = tabY[0] + UNIT_SIZE;
-                break;
-            case 'R':
-                tabX[0] = tabX[0] + UNIT_SIZE;
-                break;
-            case 'L':
-                tabX[0] = tabX[0] - UNIT_SIZE;
-                break;
+        switch (direction) { // animowanie głowy węża
+            case 'U' -> tabY[0] = tabY[0] - UNIT_SIZE;
+            case 'D' -> tabY[0] = tabY[0] + UNIT_SIZE;
+            case 'R' -> tabX[0] = tabX[0] + UNIT_SIZE;
+            case 'L' -> tabX[0] = tabX[0] - UNIT_SIZE;
         }
     }
     public void lookForApple(){
@@ -169,16 +223,19 @@ public class Panel extends JPanel implements ActionListener {
             if((tabX[0] == tabX[i] && tabY[0] == tabY[i])){ // koniec gry jak glowa weza uderzy w ogon
                 running = false;
             }
+            if(tabX[i] == appleX && tabY[i] == appleY) {
+                newApple();
+            }
             if(tabX[0]< 0){ // kolizja z lewa krawedzia
                 running = false;
             }
-            if(tabX[0]> SCREEN_WIDTH){ // kolizja z prawa krawedzia
+            if(tabX[0]>= SCREEN_WIDTH){ // kolizja z prawa krawedzia
                 running = false;
             }
             if(tabY[0]<0){ // kolizja z gorna krawedzia
                 running = false;
             }
-            if(tabY[0]>SCREEN_HEIGHT){ // kolizja z dolna krawedzia
+            if(tabY[0]>=SCREEN_HEIGHT){ // kolizja z dolna krawedzia
                 running = false;
             }
             if(!running){
@@ -200,7 +257,12 @@ public class Panel extends JPanel implements ActionListener {
         g.setColor(Color.GREEN); // wciśnij spację, by zagrać jeszcze raz
         g.setFont(new Font("Comic Sans MS",Font.BOLD,22));
         FontMetrics metrics3 = getFontMetrics(g.getFont());
-        g.drawString("Press Esc to Try again!",(SCREEN_WIDTH - metrics3.stringWidth("Press Esc to Try again!"))/2,SCREEN_HEIGHT-250 );
+        g.drawString("Press SPACE to Try again!",(SCREEN_WIDTH - metrics3.stringWidth("Press SPACE to Try again!"))/2,SCREEN_HEIGHT-250 );
+
+        g.setColor(Color.GREEN); // wciśnij spację, by zagrać jeszcze raz
+        g.setFont(new Font("Comic Sans MS",Font.BOLD,16));
+        FontMetrics metrics4 = getFontMetrics(g.getFont());
+        g.drawString("Press Esc to back to MENU!",(SCREEN_WIDTH - metrics4.stringWidth("Press Esc to back to MENU!"))/2,SCREEN_HEIGHT-150 );
 
         g.setColor(new Color(48, 217, 36, 255)); //podpis autora
         g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
@@ -208,14 +270,6 @@ public class Panel extends JPanel implements ActionListener {
         g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH-305),SCREEN_HEIGHT-10); // for author
     }
 
-    public void startNewGame(Graphics g){
-
-        g.setColor(new Color(48, 217, 36, 255)); //podpis autora
-        g.setFont(new Font("Comic Sans MS",Font.BOLD,13));
-        FontMetrics metrics2 = getFontMetrics(g.getFont());
-        g.drawString("Game made by Auqherus na zaliczenie projektu",(SCREEN_WIDTH+UNIT_SIZE*9-metrics2.stringWidth("Game made by Auqherus"))/2,SCREEN_HEIGHT-10);
-
-    }
     @Override
     public void actionPerformed(ActionEvent e) {  // główne metody metody kolizji, jabłka i ruchu
 
@@ -224,12 +278,17 @@ public class Panel extends JPanel implements ActionListener {
             lookForApple();
             checkCollisions();
         }
-        repaint();
 
+        repaint();
     }
+
+
+
     public class MyKeyAdapter extends KeyAdapter{
         @Override
-        public void keyPressed(KeyEvent e){     // przypisanie ruchów węża do klawiatury
+
+        public void keyPressed(KeyEvent e){// przypisanie ruchów węża do klawiatury
+
             switch (e.getKeyCode()){
                 case KeyEvent.VK_LEFT:
                     if(direction != 'R'){
@@ -258,7 +317,7 @@ public class Panel extends JPanel implements ActionListener {
                     }
                     break;
 
-                case KeyEvent.VK_ESCAPE:
+                case KeyEvent.VK_SPACE:
                     if(isGameOver){
                        eatenApples = 0;
                        bodyParts = 2;
@@ -267,6 +326,29 @@ public class Panel extends JPanel implements ActionListener {
                     }
                     break;
 
+                case KeyEvent.VK_ESCAPE:
+                    if(isGameOver && !isGameMenu){
+                        eatenApples = 0;
+                        bodyParts = 2;
+                        isGameOver = false;
+                        isGameMenu = true;
+                        startGame();
+                    }
+                    break;
+
+                case KeyEvent.VK_P:  // PAUSE
+                    if(!pause && running){
+
+                        pause = true;
+
+                    }
+                    else if(pause && running){
+                        timer.start();
+                        pause = false;
+
+
+                    }
+                    break;
             }
 
         }
